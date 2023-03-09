cd84a7f1dbd0669cd2cd52bb238490d59c079725
Andrew <107002944+asweinrich@users.noreply.github.com>
2023-03-09 12:46:39 -0600

    Update cache.js

--- cache.js
+++ cache.js
@@ -1,9 +1,11 @@
-export const cache = {}
+const cache = {
+  cache: {},
+  get(key) {
+    return this.cache[key];
+  },
+  set(key, val) {
+    this.cache[key] = val;
+  },
+};
 
-export function get(key) {
-    return this.cache[key]
-}
-
-export function set(key, val) {
-    this.cache[key] = val
-}
+export default cache;
