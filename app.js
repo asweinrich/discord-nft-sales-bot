e5a609db896ab52760c70e928c407533d6490a97
Andrew <107002944+asweinrich@users.noreply.github.com>
2023-03-14 13:37:13 -0500

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ import {
   ButtonStyleTypes,
 } from 'discord-interactions';
 import { VerifyDiscordRequest, getRandomEmoji, DiscordRequest } from './utils.js';
+import { CHALLENGE_COMMAND, TEST_COMMAND, HasGuildCommands } from './commands.js';
 import cache from './cache.js';
 import _ from 'lodash';
 import { ethers } from 'ethers';
@@ -28,7 +29,55 @@ client.once(Events.ClientReady, c => {
 
 client.login(process.env.DISCORD_TOKEN);
 
+// Create an express app
+const app = express();
+// Get port, or default to 3000
+const PORT = process.env.PORT || 3000;
+// Parse request body and verifies incoming requests using discord-interactions package
+app.use(express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) }));
 
+/**
+ * Interactions endpoint URL where Discord will send HTTP requests
+ */
+
+app.post('/interactions', async function (req, res) {
+  // Interaction type and data
+  const { type, id, data } = req.body;
+
+  //
+  if (type === InteractionType.PING) {
+    return res.send({ type: InteractionResponseType.PONG });
+  }
+
+  // Handle slash command requests
+
+  if (type === InteractionType.APPLICATION_COMMAND) {
+    const { name } = data;
+
+    // "test" guild command
+    if (name === 'test') {
+      // Send a message into the channel where command was triggered from
+      return res.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          // Fetches a random emoji to send from a helper function
+          content: 'hello world ' + getRandomEmoji(),
+        },
+      });
+    }
+ 
+  }
+});
+
+
+app.listen(PORT, () => {
+  console.log('Listening on port', PORT);
+  // Check if guild commands from commands.js are installed (if not, install them)
+  HasGuildCommands(process.env.APP_ID, process.env.GUILD_ID, [
+    TEST_COMMAND,
+    CHALLENGE_COMMAND,
+  ]);
+}); 
 
 
 // loadJSON method to open the JSON file.
